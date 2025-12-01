643cecef14569d505abe76c58bcf2de02f650dd2
nombre-original <diana.ortola@mail.ucv.es>
2025-12-01 17:00:24 +0100

    Upload006.1_Estancados
    controlador funciona, falta revisar límite derecho

--- Assets/Scripts/Juego_2/SCR_PlayerLVL2.cs
+++ Assets/Scripts/Juego_2/SCR_PlayerLVL2.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 
-public class SCR_PlayerLVL2 : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class SCR_PlayerLVL2 : MonoBehaviour{
+    public float fuerzaGiro;
+    private Quaternion posicionInicial;
+    public float limiteIzq, limiteDer;
+    public bool izq;
+
+    void Start(){
+        fuerzaGiro = 0.2f;
+        posicionInicial = transform.rotation;
+        limiteIzq = 35f;
+        limiteDer = 105f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        //controlador
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+            transform.Rotate(0f, 0f, fuerzaGiro, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+            transform.Rotate(0f, 0f, -fuerzaGiro, Space.World);
+        }
+
+        Limites();
+    }
+
+    private void Limites(){
+        //ESTO ESTÁ POR REVISAR, falla el límite derecho
+
+        //esto está porque eulerangles no cuenta num negativos
+        if (transform.eulerAngles.z < 180){
+            izq = true;
+        }
+        if (transform.eulerAngles.z >= 180){
+            izq = false;
+        }
+
+        //límite
+        if (izq && transform.eulerAngles.z > limiteIzq){
+            Debug.Log("Límite atravesado izq");
+            //transform.rotation = posicionInicial;
+            //cambiar anim a caída
+        }
+        if (!izq && transform.eulerAngles.z < limiteDer){
+            Debug.Log("Límite atravesado der");
+        }
     }
 }
