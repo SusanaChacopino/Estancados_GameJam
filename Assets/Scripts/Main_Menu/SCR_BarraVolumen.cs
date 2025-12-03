800aa67041f9091d32bd70933a040f007f5e834d
SusanaChacopino <chacopino@hotmail.es>
2025-12-03 16:50:52 +0100

    Upload008_Estancados
    Menu ajustes maquetado, audio puesto, pero todavia no se regula

---
+++ Assets/Scripts/Main_Menu/SCR_BarraVolumen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    public Slider soundSlider;
+    public AudioMixer masterMixer;
+
+    void Start()
+    {
+        float saved = PlayerPrefs.GetFloat("SavedMasterVolume", 100f);
+        SetVolume(saved);
+    }
+
+    public void SetVolume(float value)
+    {
+        if (value < 1f)
+            value = 0.001f;
+
+        RefreshSlider(value);
+
+        PlayerPrefs.SetFloat("SavedMasterVolume", value);
+
+        float db = Mathf.Log10(value / 100f) * 20f;
+        masterMixer.SetFloat("MasterVolume", db);
+    }
+
+    public void SetVolumeFromSlider()
+    {
+        SetVolume(soundSlider.value);
+    }
+
+    void RefreshSlider(float value)
+    {
+        soundSlider.value = value;
+    }
+}
