2a55ae57c3494bb36137ee19b1426ac2ddc9ac19
SusanaChacopino <chacopino@hotmail.es>
2025-12-18 20:40:55 +0100

    Upload029_Estancados
    Barra de juego 2 terminada y Sprites de teclas de juego 2 puestos

---
+++ Assets/Scripts/Juego_2/SCR_Barra.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SCR_Barra : MonoBehaviour
+{
+    public SCR_Puntos puntos;
+    public RectTransform spriteRana;
+    public RectTransform mascara;
+
+    public float puntosMax = 20f;
+    public float spriteRanaPosXMax = 212f;
+    public float mascaraWidthMax = 100f;
+
+    float spriteRanaPosXInicio;
+
+    void Start()
+    {
+        spriteRanaPosXInicio = spriteRana.anchoredPosition.x;
+    }
+
+    void Update()
+    {
+        //Convierte los puntos en un valor entre 0 y 1 dependiendo del maximo
+        float ValorPuntos = Mathf.Clamp01(puntos.puntos / puntosMax);
+
+        //mover el sprite de la Rana
+        Vector2 pos = spriteRana.anchoredPosition;
+        pos.x = Mathf.Lerp(spriteRanaPosXInicio, spriteRanaPosXMax, ValorPuntos);
+        spriteRana.anchoredPosition = pos;
+
+        //cambiar ancho de la mascara
+        Vector2 width = mascara.sizeDelta;
+        width.x = Mathf.Lerp(0, mascaraWidthMax, ValorPuntos);
+        mascara.sizeDelta = width;
+    }
+}
